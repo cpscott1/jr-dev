7f250b549f03958fab89f28f847005f0694df294
cperry <>
2023-10-25 11:59:43 -0500

    adding the header section

---
+++ resources/views/home.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>JR Dev Kids</title>
+
+        <!-- Fonts -->
+       
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{ asset('css/reset.css')}}">
+        <link rel="stylesheet" href="{{ asset('css/style.css')}}">
+       
+    </head>
+    <body>
+    <div>
+        <header class="header-container">
+            <div class="header-grid">
+                <div class="header-logo">
+                    Logo
+                </div>
+                <div class="header-item-1">
+                Item 1
+                </div>
+                <div class="header-item-2">
+                Item 2
+                </div>
+            </div>
+           
+        </header>
+    </div>
+    </body>
+</html>
